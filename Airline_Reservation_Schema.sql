2090955d6fb63d3c15eefa34826ef10f061ec801
Fahad Ahmad <fahadahmad8889@gmail.com>
2026-05-07 00:41:22 +0500

    Update Airline_Reservation_Schema.sql

--- Airline_Reservation_Schema.sql
+++ Airline_Reservation_Schema.sql
@@ -1,69 +1,16 @@
 
+
+
+
 -- =====================================================
 -- CREATE AIRPORTS TABLE
 -- =====================================================
 
 CREATE TABLE Airports(
-	Airport_ID int IDENTITY(1,1) NOT NULL,
-	Airport_Name varchar(100) NOT NULL,
-	City varchar(50) NOT NULL,
-	Country varchar(50) NOT NULL
-);
-
-
-
--- =====================================================
--- CREATE BOOKINGS TABLE
--- =====================================================
-
-CREATE TABLE Bookings(
-	Booking_ID int IDENTITY(1,1) NOT NULL,
-	Passenger_ID int NOT NULL,
-	Flight_ID int NOT NULL,
-	Seat_ID int NOT NULL,
-	Booking_DateTime datetime NULL
-);
-
-
-
--- =====================================================
--- CREATE EMPLOYEES TABLE
--- =====================================================
-
-CREATE TABLE Employees(
-	Employee_ID int IDENTITY(1,1) NOT NULL,
-	Name varchar(50) NOT NULL,
-	Role varchar(25) NOT NULL,
-	Email varchar(30) NULL
-);
-
-
-
--- =====================================================
--- CREATE FLIGHT_CREW TABLE
--- =====================================================
-
-CREATE TABLE Flight_Crew(
-	Crew_ID int IDENTITY(1,1) NOT NULL,
-	Flight_ID int NOT NULL,
-	Employee_ID int NOT NULL
-);
-
-
-
--- =====================================================
--- CREATE FLIGHTS TABLE
--- =====================================================
-
-CREATE TABLE Flights(
-	Flight_ID int IDENTITY(1,1) NOT NULL,
-	Flight_Number varchar(10) NOT NULL,
-	Departure_Airport_ID int NOT NULL,
-	Arrival_Airport_ID int NOT NULL,
-	Departure_DateTime datetime NOT NULL,
-	Arrival_DateTime datetime NOT NULL,
-	Aircraft_Type varchar(50) NOT NULL,
-	Status varchar(20) NULL
+	Airport_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Airport_Name VARCHAR(100) NOT NULL,
+	City VARCHAR(50) NOT NULL,
+	Country VARCHAR(50) NOT NULL
 );
 
 
@@ -73,12 +20,48 @@ CREATE TABLE Flights(
 -- =====================================================
 
 CREATE TABLE Passengers(
-	Passenger_ID int IDENTITY(1,1) NOT NULL,
-	First_Name varchar(30) NOT NULL,
-	Last_Name varchar(30) NOT NULL,
-	Phone_Number varchar(15) NOT NULL,
-	Email varchar(50) NOT NULL,
-	Passport_Number varchar(30) NOT NULL
+	Passenger_ID INT IDENTITY(1,1) PRIMARY KEY,
+	First_Name VARCHAR(30) NOT NULL,
+	Last_Name VARCHAR(30) NOT NULL,
+	Phone_Number VARCHAR(15) NOT NULL,
+	Email VARCHAR(50) NOT NULL,
+	Passport_Number VARCHAR(30) NOT NULL
+);
+
+
+
+-- =====================================================
+-- CREATE EMPLOYEES TABLE
+-- =====================================================
+
+CREATE TABLE Employees(
+	Employee_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL,
+	Role VARCHAR(25) NOT NULL,
+	Email VARCHAR(30) NULL
+);
+
+
+
+-- =====================================================
+-- CREATE FLIGHTS TABLE
+-- =====================================================
+
+CREATE TABLE Flights(
+	Flight_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Flight_Number VARCHAR(10) NOT NULL,
+	Departure_Airport_ID INT NOT NULL,
+	Arrival_Airport_ID INT NOT NULL,
+	Departure_DateTime DATETIME NOT NULL,
+	Arrival_DateTime DATETIME NOT NULL,
+	Aircraft_Type VARCHAR(50) NOT NULL,
+	Status VARCHAR(20) NULL,
+
+	FOREIGN KEY (Departure_Airport_ID)
+	REFERENCES Airports(Airport_ID),
+
+	FOREIGN KEY (Arrival_Airport_ID)
+	REFERENCES Airports(Airport_ID)
 );
 
 
@@ -88,11 +71,55 @@ CREATE TABLE Passengers(
 -- =====================================================
 
 CREATE TABLE Seats(
-	Seat_ID int IDENTITY(1,1) NOT NULL,
-	Flight_ID int NOT NULL,
-	Seat_Number varchar(20) NOT NULL,
-	class varchar(20) NOT NULL,
-	Available char(1) NOT NULL
+	Seat_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Flight_ID INT NOT NULL,
+	Seat_Number VARCHAR(20) NOT NULL,
+	Class VARCHAR(20) NOT NULL,
+	Available CHAR(1) NOT NULL,
+
+	FOREIGN KEY (Flight_ID)
+	REFERENCES Flights(Flight_ID)
+);
+
+
+
+-- =====================================================
+-- CREATE BOOKINGS TABLE
+-- =====================================================
+
+CREATE TABLE Bookings(
+	Booking_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Passenger_ID INT NOT NULL,
+	Flight_ID INT NOT NULL,
+	Seat_ID INT NOT NULL,
+	Booking_DateTime DATETIME NULL,
+
+	FOREIGN KEY (Passenger_ID)
+	REFERENCES Passengers(Passenger_ID),
+
+	FOREIGN KEY (Flight_ID)
+	REFERENCES Flights(Flight_ID),
+
+	FOREIGN KEY (Seat_ID)
+	REFERENCES Seats(Seat_ID)
+);
+
+
+
+-- =====================================================
+-- CREATE FLIGHT_CREW TABLE
+-- =====================================================
+
+CREATE TABLE Flight_Crew(
+	Crew_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Flight_ID INT NOT NULL,
+	Employee_ID INT NOT NULL,
+
+	FOREIGN KEY (Flight_ID)
+	REFERENCES Flights(Flight_ID),
+
+	FOREIGN KEY (Employee_ID)
+	REFERENCES Employees(Employee_ID)
 );
 
 
@@ -123,10 +150,10 @@ INSERT INTO Passengers
 	Passport_Number
 )
 VALUES
-('Ali', 'Khan', '+9203001234567', 'ali@gmail.com', 'PK123456'),
-('Ahmed', 'Raza', '+9203111234567', 'ahmed@gmail.com', 'PK654321'),
-('Sara', 'Iqbal', '+9203221234567', 'sara@gmail.com', 'PK987654'),
-('Ayesha', 'Noor', '+9203331234567', 'ayesha@gmail.com', 'PK741852');
+('Ali', 'Khan', '03001234567', 'ali@gmail.com', 'PK123456'),
+('Ahmed', 'Raza', '03111234567', 'ahmed@gmail.com', 'PK654321'),
+('Sara', 'Iqbal', '03221234567', 'sara@gmail.com', 'PK987654'),
+('Ayesha', 'Noor', '03331234567', 'ayesha@gmail.com', 'PK741852');
 
 
 
@@ -196,7 +223,7 @@ INSERT INTO Seats
 (
 	Flight_ID,
 	Seat_Number,
-	class,
+	Class,
 	Available
 )
 VALUES
@@ -241,6 +268,100 @@ VALUES
 (1, 2),
 (2, 3),
 (3, 4);
+
+
+
+-- =====================================================
+-- STORED PROCEDURE:
+-- ADD NEW PASSENGER
+-- =====================================================
+
+CREATE PROCEDURE AddPassenger
+(
+    @First_Name VARCHAR(30),
+    @Last_Name VARCHAR(30),
+    @Phone_Number VARCHAR(15),
+    @Email VARCHAR(50),
+    @Passport_Number VARCHAR(30)
+)
+AS
+BEGIN
+    INSERT INTO Passengers
+    (
+        First_Name,
+        Last_Name,
+        Phone_Number,
+        Email,
+        Passport_Number
+    )
+    VALUES
+    (
+        @First_Name,
+        @Last_Name,
+        @Phone_Number,
+        @Email,
+        @Passport_Number
+    );
+END;
+
+
+
+-- =====================================================
+-- STORED PROCEDURE:
+-- VIEW ALL FLIGHTS
+-- =====================================================
+
+CREATE PROCEDURE ViewFlights
+AS
+BEGIN
+    SELECT * FROM Flights;
+END;
+
+
+
+-- =====================================================
+-- STORED PROCEDURE:
+-- BOOK FLIGHT
+-- =====================================================
+
+CREATE PROCEDURE BookFlight
+(
+    @Passenger_ID INT,
+    @Flight_ID INT,
+    @Seat_ID INT
+)
+AS
+BEGIN
+    INSERT INTO Bookings
+    (
+        Passenger_ID,
+        Flight_ID,
+        Seat_ID,
+        Booking_DateTime
+    )
+    VALUES
+    (
+        @Passenger_ID,
+        @Flight_ID,
+        @Seat_ID,
+        GETDATE()
+    );
+END;
+
+
+
+-- =====================================================
+-- STORED PROCEDURE:
+-- VIEW AVAILABLE SEATS
+-- =====================================================
+
+CREATE PROCEDURE AvailableSeats
+AS
+BEGIN
+    SELECT *
+    FROM Seats
+    WHERE Available = 'Y';
+END;
 
 
 
